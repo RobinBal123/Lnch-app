51178590f9f7e25f287ac282ba6cfc4f6a7066c1
Bal <robin.bal@capgemini.com>
2025-02-28 11:29:51 +0100

    Sparar innan rebase

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2dbac3c5e4ae6d9d5351f322a903369f81902c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9aed2a9244b92c28c548abfd1f164aa8f776361")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
